6ffe24b8f07a7a0bc46542e14e2a9535bd4399ef
Pham Tan Minh Tien <55906231+phamtanminhtien@users.noreply.github.com>
2026-03-14 15:15:45 +0700

    Refactor lower.rs into smaller modules

---
+++ src/compiler/ir/lower/expr.rs
@@ -0,0 +1,253 @@
+use crate::compiler::ast::Expr;
+use crate::compiler::ir::instr::{Operand};
+use crate::compiler::sema::ty::Type;
+use super::Lowerer;
+
+impl Lowerer {
+    pub fn lower_expr(&mut self, expr: Expr) -> Operand {
+        match expr {
+            Expr::Number(n, _) => {
+                self.last_expr_ty = Type::Int32;
+                Operand::Constant(n as i64)
+            }
+            Expr::Null(_) => {
+                self.last_expr_ty = Type::Null;
+                Operand::Constant(0)
+            }
+            Expr::Template(_, _) => {
+                todo!("Template lowering")
+            }
+            Expr::Await(_, _) => todo!("Await in IR lower"),
+            Expr::ArrayLiteral(_, _) => todo!("Array literals in IR lower"),
+            Expr::Error(_) => panic!("Compiler bug: error node in IR lower"),
+            Expr::StringLiteral(s, _) => {
+                let name = format!("str_{}", self.globals.len());
+                self.globals.push((name.clone(), s));
+                self.last_expr_ty = Type::String;
+                // Treat strings as pointers in IR for now
+                self.builder.call(
+                    "aura_get_string".to_string(),
+                    vec![Operand::Constant(self.globals.len() as i64 - 1)],
+                )
+            }
+            Expr::Variable(name, _) => {
+                if let Some((ptr_reg, cls_name, ty)) = self.mem_vars.get(&name).cloned() {
+                    self.last_expr_ty = ty.unwrap_or_else(|| {
+                        if let Some(cls) = cls_name {
+                            Type::Class(cls)
+                        } else {
+                            Type::Unknown
+                        }
+                    });
+                    let dest = self.builder.new_reg();
+                    self.builder
+                        .emit(crate::compiler::ir::instr::Instruction::Load(
+                            dest,
+                            Operand::Value(ptr_reg),
+                            0,
+                        ));
+                    Operand::Value(dest)
+                } else if self.class_structures.contains_key(&name) {
+                    self.last_expr_ty = Type::Class(name);
+                    Operand::Constant(0) // Class constant is 0
+                } else {
+                    panic!("Undefined variable {}", name);
+                }
+            }
+            Expr::BinaryOp(left, op, right, _) => {
+                let lhs = self.lower_expr(*left);
+                let lhs_ty = self.last_expr_ty.clone();
+                let rhs = self.lower_expr(*right);
+                // Binary ops currently assume numeric for simplicity
+                self.last_expr_ty = if op == "=="
+                    || op == "!="
+                    || op == "<"
+                    || op == "<="
+                    || op == ">"
+                    || op == ">="
+                {
+                    Type::Boolean
+                } else {
+                    lhs_ty
+                };
+                match op.as_str() {
+                    "+" => self.builder.add(lhs, rhs),
+                    "-" => self.builder.sub(lhs, rhs),
+                    "*" => self.builder.mul(lhs, rhs),
+                    "/" => self.builder.div(lhs, rhs),
+                    "%" => self.builder.rem(lhs, rhs),
+                    "==" => self.builder.eq(lhs, rhs),
+                    "!=" => self.builder.ne(lhs, rhs),
+                    "<" => self.builder.lt(lhs, rhs),
+                    "<=" => self.builder.le(lhs, rhs),
+                    ">" => self.builder.gt(lhs, rhs),
+                    ">=" => self.builder.ge(lhs, rhs),
+                    _ => panic!("Unsupported operator {}", op),
+                }
+            }
+            Expr::Call(name, _, args, _) => {
+                let (_, r_ty) = self
+                    .function_tys
+                    .get(&name)
+                    .cloned()
+                    .unwrap_or((vec![], Type::Unknown));
+                let ir_args = args.into_iter().map(|a| self.lower_expr(a)).collect();
+                self.last_expr_ty = r_ty;
+                self.builder.call(name, ir_args)
+            }
+            Expr::Assign(name, value, _) => {
+                let val_op = self.lower_expr(*value);
+                let (ptr_reg, _, _) = self
+                    .mem_vars
+                    .get(&name)
+                    .cloned()
+                    .expect("Undefined variable");
+                self.builder
+                    .emit(crate::compiler::ir::instr::Instruction::Store(
+                        val_op.clone(),
+                        Operand::Value(ptr_reg),
+                        0,
+                    ));
+                val_op
+            }
+            Expr::New(class_name, _, args, _) => {
+                let size = self
+                    .class_layouts
+                    .get(&class_name)
+                    .map(|l| l.size)
+                    .unwrap_or(0);
+                let obj_reg = self.builder.call(
+                    "aura_alloc".to_string(),
+                    vec![Operand::Constant(size as i64)],
+                );
+
+                // Call constructor if exists
+                let ctor_name = format!("{}_ctor", class_name);
+                let mut ctor_args = vec![obj_reg.clone()];
+                ctor_args.extend(args.into_iter().map(|a| self.lower_expr(a)));
+                self.builder.call(ctor_name, ctor_args);
+
+                self.last_expr_ty = Type::Class(class_name);
+                obj_reg
+            }
+            Expr::This(_) => {
+                let (ptr_reg, _, _) = self
+                    .mem_vars
+                    .get("this")
+                    .cloned()
+                    .expect("this not in scope");
+                self.last_expr_ty = self
+                    .current_class
+                    .as_ref()
+                    .map(|c| Type::Class(c.clone()))
+                    .unwrap_or(Type::Unknown);
+                let dest = self.builder.new_reg();
+                self.builder
+                    .emit(crate::compiler::ir::instr::Instruction::Load(
+                        dest,
+                        Operand::Value(ptr_reg),
+                        0,
+                    ));
+                Operand::Value(dest)
+            }
+            Expr::MemberAccess(obj, field, _, _) => {
+                let obj_op = self.lower_expr(*obj);
+                let obj_ty = self.last_expr_ty.clone();
+                if let Type::Class(cls_name) = obj_ty {
+                    let offset = self.get_field_offset(&cls_name, &field);
+                    let field_ty = self
+                        .class_structures
+                        .get(&cls_name)
+                        .and_then(|s| s.get(&field))
+                        .cloned()
+                        .unwrap_or(Type::Unknown);
+                    let dest = self.builder.new_reg();
+                    self.builder
+                        .emit(crate::compiler::ir::instr::Instruction::Load(
+                            dest, obj_op, offset,
+                        ));
+                    self.last_expr_ty = field_ty;
+                    Operand::Value(dest)
+                } else {
+                    panic!("Member access on non-class type {:?}", obj_ty);
+                }
+            }
+            Expr::MemberAssign(obj, field, value, _, _) => {
+                let obj_op = self.lower_expr(*obj);
+                let obj_ty = self.last_expr_ty.clone();
+                let val_op = self.lower_expr(*value);
+                if let Type::Class(cls_name) = obj_ty {
+                    let offset = self.get_field_offset(&cls_name, &field);
+                    self.builder
+                        .emit(crate::compiler::ir::instr::Instruction::Store(
+                            val_op.clone(),
+                            obj_op.clone(),
+                            offset,
+                        ));
+                    self.builder
+                        .emit(crate::compiler::ir::instr::Instruction::WriteBarrier(
+                            obj_op,
+                            val_op.clone(),
+                        ));
+                    val_op
+                } else {
+                    panic!("Member assign on non-class type {:?}", obj_ty);
+                }
+            }
+            Expr::MethodCall(obj, method, _, args, _) => {
+                let obj_op = self.lower_expr(*obj);
+                let obj_ty = self.last_expr_ty.clone();
+                let mut is_static = false;
+                let mangled_name = if let Type::Class(cls_name) = obj_ty {
+                    if let Some(statics) = self.static_methods.get(&cls_name) {
+                        if statics.contains(&method) {
+                            is_static = true;
+                        }
+                    }
+                    format!("{}_{}", cls_name, method)
+                } else {
+                    method
+                };
+
+                let mut ir_args = Vec::new();
+                if !is_static {
+                    ir_args.push(obj_op);
+                }
+                ir_args.extend(args.into_iter().map(|a| self.lower_expr(a)));
+                self.last_expr_ty = Type::Unknown; // TODO: Lookup method return type
+                self.builder.call(mangled_name, ir_args)
+            }
+            Expr::UnaryOp(op, expr, _) => {
+                let val = self.lower_expr(*expr);
+                if op == "-" {
+                    self.builder.sub(Operand::Constant(0), val)
+                } else {
+                    val
+                }
+            }
+            Expr::TypeTest(expr, ty_expr, _) => {
+                let _target_ty = self.resolve_type(ty_expr);
+                let val_op = self.lower_expr(*expr);
+                self.last_expr_ty = Type::Boolean;
+
+                // Let's just emit a call to aura_check_tag for now
+                self.builder.call(
+                    "aura_check_tag".to_string(),
+                    vec![val_op, Operand::Constant(1)],
+                )
+            }
+            Expr::Throw(expr, _) => {
+                let val = self.lower_expr(*expr);
+                self.builder.call("aura_throw".to_string(), vec![val]);
+                Operand::Constant(0)
+            }
+            Expr::Index(obj, index, _) => {
+                let obj_op = self.lower_expr(*obj);
+                let index_op = self.lower_expr(*index);
+                // Lower to intrinsic call for now
+                self.builder
+                    .call("__arr_get".to_string(), vec![obj_op, index_op])
+            }
+        }
+    }
+}
